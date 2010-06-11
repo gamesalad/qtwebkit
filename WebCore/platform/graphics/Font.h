ad6104996d73c62d831a04a21fdd8fe5cf30e152
jocelyn.turcotte <jocelyn.turcotte@nokia.com>
2010-06-11 09:58:29 +0000

    Move the text code path detection code from FontFastPath.cpp to Font.cpp. https://bugs.webkit.org/show_bug.cgi?id=40077
    Reviewed by Simon Hausmann.
    
    This is a patch preliminary to the implementation of the
    simple font code path for the Qt port.
    
    * platform/graphics/Font.cpp:
    (WebCore::Font::setCodePath):
    (WebCore::Font::codePath):
    * platform/graphics/Font.h:
    * platform/graphics/FontFastPath.cpp:
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@61001 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/Font.h
+++ WebCore/platform/graphics/Font.h
@@ -152,9 +152,7 @@ public:
     static void setShouldUseSmoothing(bool);
     static bool shouldUseSmoothing();
 
-#if USE(FONT_FAST_PATH)
     enum CodePath { Auto, Simple, Complex, SimpleWithGlyphOverflow };
-#endif
 
 private:
 #if ENABLE(SVG_FONTS)
@@ -166,7 +164,6 @@ private:
 #endif
 
 #if USE(FONT_FAST_PATH)
-    CodePath codePath(const TextRun&) const;
     void drawSimpleText(GraphicsContext*, const TextRun&, const FloatPoint&, int from, int to) const;
     void drawGlyphs(GraphicsContext*, const SimpleFontData*, const GlyphBuffer&, int from, int to, const FloatPoint&) const;
     void drawGlyphBuffer(GraphicsContext*, const GlyphBuffer&, const TextRun&, const FloatPoint&) const;
@@ -177,6 +174,7 @@ private:
     static bool canReturnFallbackFontsForComplexText();
 #endif
 
+    CodePath codePath(const TextRun&) const;
     void drawComplexText(GraphicsContext*, const TextRun&, const FloatPoint&, int from, int to) const;
     float floatWidthForComplexText(const TextRun&, HashSet<const SimpleFontData*>* fallbackFonts = 0, GlyphOverflow* = 0) const;
     int offsetForPositionForComplexText(const TextRun&, int position, bool includePartialGlyphs) const;
@@ -186,11 +184,11 @@ private:
 
 public:
     // Useful for debugging the different font rendering code paths.
-#if USE(FONT_FAST_PATH)
     static void setCodePath(CodePath);
     static CodePath codePath();
     static CodePath s_codePath;
 
+#if USE(FONT_FAST_PATH)
     static const uint8_t gRoundingHackCharacterTable[256];
     static bool isRoundingHackCharacter(UChar32 c)
     {
